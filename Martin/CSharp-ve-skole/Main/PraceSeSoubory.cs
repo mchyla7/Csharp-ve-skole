8f2cb0c938bab55bec045a1835e9c9f2d02f4cb4
mchyla7 <mchyla7@gmail.com>
2024-04-22 09:08:39 +0200

    ➕ Přidáno první procvičování se soubory

---
+++ Martin/CSharp-ve-skole/Main/PraceSeSoubory.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;    // Musíme přidat
+
+namespace CSharp_ve_skole
+{
+    class PraceSeSoubory
+    {
+        public static void CteniZeSouboru()
+        {
+            string cesta = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\CSharp-ve-skole\\Martin\\CSharp-ve-skole\\Main\\Vedlejší soubory\\text.txt";
+
+            string t = File.ReadAllText(cesta);
+
+            Console.WriteLine(t);
+            Console.ReadLine();
+        }
+
+        // Program načte text ze souboru
+        // Program vrátí počet znaků v textu, ale bez mezer
+
+        public static void PocetZnakuvSouboru()
+        {
+            string cesta = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\CSharp-ve-skole\\Martin\\CSharp-ve-skole\\Main\\Vedlejší soubory\\text.txt";
+
+            string t = File.ReadAllText(cesta);
+
+            Console.WriteLine(t.Replace(" ", "").Length);
+            Console.ReadLine();
+        }
+
+        // Program načte soubor
+        // Soubor zjístí počet výskytů znaků "@"
+
+        public static void PocetZavinacuVSouboru()
+        {
+            string cesta = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\CSharp-ve-skole\\Martin\\CSharp-ve-skole\\Main\\Vedlejší soubory\\text.txt";
+            string text = File.ReadAllText(cesta);
+            int pocet = 0;
+
+            foreach (char znak in text)
+            {
+                if (znak == '@')
+                {
+                    pocet++;
+                }
+            }
+
+            Console.WriteLine("Počet @ je {0}", pocet);
+            Console.ReadLine();
+        }
+    }
+}
--- Martin/CSharp-ve-skole/Main/Start.cs
+++ Martin/CSharp-ve-skole/Main/Start.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +9,7 @@ namespace CSharp_ve_skole
     {
         static void Main(string[] args)
         {
-            MetodyTextu.VelkeNaMalaMalaNaVelka();
+            PraceSeSoubory.PocetZnakuvSouboru();
         }
     }
 }
